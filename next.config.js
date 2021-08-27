19ff821d64da51c4eccb924532a5205157ebd1bb
CK Lam <ckhk0621@gmail.com>
2021-08-27 11:24:53 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  basePath: '/',
+  env: {
+    // USER_AUTH_KEY: process.env.USER_AUTH_KEY,
+    USER_URL: {},
+  },
+  exportPathMap: function () {
+    return {
+        '/': { page: '/' }
+    }
+  },
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return `${process.env.BUILD_ID}_${new Date().getTime()}`;
+    } else {
+      return `next_${new Date().getTime()}`;
+    }
+  },
 }
