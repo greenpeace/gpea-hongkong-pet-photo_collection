ed6eb7ddee62b10200718de01ea251abbb123492
KFC <chris.chan@greenpeace.org>
2021-09-10 10:06:51 +0800

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,24 @@ import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { wrapper } from 'store/configureStore';
 import { useDispatch } from 'react-redux';
 import { auth } from 'components/utils';
-
-import '/styles/index.scss';
+import Router from 'next/router';
+import nProgress from 'nprogress';
 
 import theme from '../theme/theme';
 import { appTheme } from '../theme/app';
 
+import 'nprogress/nprogress.css';
+import '/styles/index.scss';
+
 import * as userActions from 'store/actions/action-types/user-actions';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 const MyApp = ({ Component, pageProps }) => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     const data = auth();
     dispatch({ type: userActions.SET_USER_SUCCESS, data: JSON.parse(data) });
