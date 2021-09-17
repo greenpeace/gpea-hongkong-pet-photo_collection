064a24007b4d2a78c575e2c82c0b84e5f2be2ff1
CK Lam <ckhk0621@gmail.com>
2021-09-17 09:22:20 +0800

    getStaticProps & getStaticPaths

--- pages/tag/[slug].js
+++ pages/tag/[slug].js
@@ -11,8 +11,29 @@ export default function Index() {
       <PageContainer>
         <ListItems/>
       </PageContainer>
+      <div>ver.0.0.1</div>
     </>
   )
 }
 
 Index.getLayout = (page) => <Wrapper>{page}</Wrapper>
+
+export async function getStaticProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
+
+// `getStaticPaths` allows the user to return a list of parameters to
+// render to HTML at build time.
+export async function getStaticPaths() {
+  return {
+    // Opt-out of the described fallback behavior
+    fallback: false,
+    paths: [
+      { params: { slug: "all" } },
+      { params: { slug: "lantauLandscape" } },
+      { params: { slug: "lantauEcology" } }
+    ]
+  };
+}
