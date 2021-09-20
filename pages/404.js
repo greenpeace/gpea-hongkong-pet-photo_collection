683817028d84272bcef311af6f85c86eda2ac593
KFC <chris.chan@greenpeace.org>
2021-09-20 15:38:54 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,6 @@
 // 404.js
 import React, { useEffect, useState, useRef } from 'react'
 import { useRouter } from 'next/router'
-import Link from 'next/link'
 
 export default function FourOhFour() {
   const router = useRouter()
@@ -11,14 +10,7 @@ export default function FourOhFour() {
 
   return (
     <>
-      <h2>404 - Page Not Found</h2>
-      <Link
-        onClick={() => {
-          router.push(`/`)
-        }}
-      >
-        Back to home
-      </Link>
+      <h1>404 - Page Not Found</h1>
     </>
   )
 }
