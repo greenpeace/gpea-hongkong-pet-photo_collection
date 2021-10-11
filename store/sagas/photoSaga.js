f2f7c09868702893ec7274f70d8df5e7bab7701b
KFC <chris.chan@greenpeace.org>
2021-10-11 15:42:32 +0800

    Use published images

--- store/sagas/photoSaga.js
+++ store/sagas/photoSaga.js
@@ -1,48 +1,66 @@
 import { call, put, select } from 'redux-saga/effects'
-import axios from "axios";
-import _ from "lodash"
+import axios from 'axios'
+import _ from 'lodash'
 import * as photoActions from 'store/actions/action-types/photo-actions'
 
 export function* getPhoto() {
   try {
-    let fetchURLs = [];
-    let photoResult = [];
+    let fetchURLs = []
+    let photoResult = []
 
-    const getPhotoTotal = yield call(() => axios.get(`${process.env.G_SHEET}/photo-collection?limit=1`)
-    .then((response) => {      
-      return response.data.totalCount
-    })
-    .catch(function (error) {
-      console.log(error);
-    }));
+    const getPhotoTotal = yield call(() =>
+      axios
+        .get(
+          `${process.env.G_SHEET}/photo-collection?q={"published": "TRUE"}&limit=1`
+        )
+        .then((response) => {
+          return response.data.totalCount
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    )
 
-
-    for (let i = 0; i < getPhotoTotal; i+=100) {
-      fetchURLs = [...fetchURLs, `${process.env.G_SHEET}/photo-collection?offset=${i}`]
+    for (let i = 0; i < getPhotoTotal; i += 100) {
+      fetchURLs = [
+        ...fetchURLs,
+        `${process.env.G_SHEET}/photo-collection?q={"published": "TRUE"}&offset=${i}`,
+      ]
     }
 
-    const allPhoto = yield call(() => axios.all(
-      fetchURLs.map(d =>axios.get(d))
+    const allPhoto = yield call(() =>
+      axios
+        .all(fetchURLs.map((d) => axios.get(d)))
+        .then(
+          axios.spread(async (...res) => {
+            await res.map((d) => photoResult.push(d.data.records))
+            return photoResult.flat(1)
+          })
+        )
+        .then((data) => {
+          const resData = {
+            records: data
+              .map((d) => ({
+                ...d,
+                qEco: d.url.replace(
+                  '/upload/',
+                  '/upload/c_fit,w_480,q_auto:low/'
+                ),
+                qBest: d.url.replace(
+                  '/upload/',
+                  '/upload/c_fit,w_1920,q_auto:best/'
+                ),
+                newTimestamp: new Date(d.timestamp).getTime(),
+              }))
+              .sort((a, b) => b.newTimestamp - a.newTimestamp),
+          }
+          return resData
+        })
+
+        .catch(function (error) {
+          console.log(error)
+        })
     )
-    .then(axios.spread(async (...res) => {
-      await res.map(d => photoResult.push(d.data.records))
-      return photoResult.flat(1)
-    }))
-    .then((data) => {
-      const resData = {
-        records: data.map(d=>({
-          ...d,
-          qEco: d.url.replace("/upload/", "/upload/c_fit,w_480,q_25/"),
-          qBest: d.url.replace("/upload/", "/upload/c_fit,w_1280,q_auto:best/"),
-          newTimestamp: new Date(d.timestamp).getTime()
-        })).sort((a,b)=> b.newTimestamp - a.newTimestamp)
-      }
-      return resData
-    })
-    
-    .catch(function (error) {
-      console.log(error);
-    }));
 
     // const photos = yield call(() => axios.get(`${process.env.G_SHEET}/photo-collection?q={"published": "TRUE"}`)
     // .then((response) => response.data)
@@ -62,7 +80,6 @@ export function* getPhoto() {
     //   console.log(error);
     // }));
 
-    yield put({ type: photoActions.SET_PHOTO_SUCCESS, data: allPhoto.records});
-  } catch (e) {
-  }
+    yield put({ type: photoActions.SET_PHOTO_SUCCESS, data: allPhoto.records })
+  } catch (e) {}
 }
